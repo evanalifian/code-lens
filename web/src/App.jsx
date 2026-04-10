d3063e94c41ae20674d3febc8125a054179ecddc
evanalifian <evanalifian@gmail.com>
2026-04-10 07:27:29 +0700

    update: change api endpoint

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://code-lens-api.vercel.app/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
